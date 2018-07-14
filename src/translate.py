976c5680be5b39857f5f97f67041f4293a8ff9b8
Jean-Christophe Galvani <jchristophegalvani@gmail.com>
2018-07-14 19:19:11 +0200

    Add comments

--- src/translate.py
+++ src/translate.py
@@ -7,9 +7,11 @@ from time import sleep
 from tqdm import tqdm
 from random import random
 
+#Load english texts
 df = pd.read_csv("../data/english_dataset.csv", index_col = False)
 
 def try_translation(text):
+    """try a translation using Google Translate API and wait between 1 and 60 seconds if the translation fails"""
     try:
         trad = Translator().translate(text = text, dest = 'fr')
     except:
@@ -22,10 +24,12 @@ def trad_list(lst):
     trad_texts = list()
     
     for text in tqdm(lst, desc = 'text'):
+        #split the text into sentences
         sentences = text.split('. ')
         trad_lst = list()
         
         for sentence in tqdm(sentences, desc = 'sentences'):
+            #translate sentence by sentence
             trad = try_translation(sentence)
 
             while trad == False:
@@ -33,6 +37,7 @@ def trad_list(lst):
                 
             trad_lst.append(trad)
         
+        #turn the list of traductions into a string and save it in trad_texts
         trad_text = ". ".join(trad_lst)
         trad_texts.append(trad_text)
         del trad_lst
@@ -41,4 +46,6 @@ def trad_list(lst):
     return trad_texts
 
 df['french'] = trad_list(df['english'].tolist())
+
+#save the result into a csv file
 df.to_csv("../data/french_trad_english_dataset.csv", index = False)
